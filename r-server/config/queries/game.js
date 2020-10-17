c2fc7a27ca9cedfb906dd0d6b63fe25eb2268792
r4ph3rd <hav4na@protonmail.com>
2020-10-17 23:16:48 +0200

    create "tags" table & subtable "tags_by_games"
    query game updated

--- r-server/config/queries/game.js
+++ r-server/config/queries/game.js
@@ -5,14 +5,15 @@ const getGames = async ({limit = 100, orderKey = 'creation_date', order = 'asc'}
 }
 
 const getGame = async (game_id) => {
+    let tags = await db.pluck('tag_id').from('tags_by_games').where('game_id', game_id.id);
     return await Promise.all([
         db.select().from('games').where('id', game_id.id),
-        db.select('tag_1', 'tag_2', 'tag_3').from('tags_by_games').where('game_id', game_id.id),
-        db.select('user_id').from('users_by_games').where('game_id', game_id.id)
+        db.pluck('label').from('tags').where('id', tags[0]).orWhere('id', tags[1]).orWhere('id', tags[2]),
+        db.select().from('users_by_games').where('game_id', game_id.id)
     ]).then( responses => {
         return {
             game_infos : responses[0][0],
-            game_tags : Object.values(responses[1][0]),
+            game_tags : Object.values(responses[1]),
             game_users_id : responses[2].map( user => user.user_id)
         }
     }) ;
--- rolline/src/views/CreateGame.vue
+++ rolline/src/views/CreateGame.vue
@@ -7,6 +7,14 @@
       <v-input name="Nom de la partie">Chroniques oubliées</v-input>
       <input-search name="Tags affiliés">Super</input-search>
 
+      <v-input name="Mode de jeu"></v-input>
+      <v-input name="Template de la fiche de personnage"></v-input>
+
+      <input-search name="Joueurs invités">Super</input-search>
+
+      <v-input name="publication" type="toggle"></v-input>
+
+      <drop-zone ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></drop-zone>
     </section>
   </div>
 </template>
@@ -15,15 +23,35 @@
 import input from '@/components/atoms/input'
 import inputSearch from '@/components/molecules/inputSearch'
 
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+
 export default {
   name: 'CreateGame',
   components:{
     'v-input': input,
-    'input-search': inputSearch
+    'input-search': inputSearch,
+    'drop-zone': vue2Dropzone
+  },
+  data(){
+    return{
+      dropzoneOptions: {
+          url: 'https://httpbin.org/post',
+          thumbnailWidth: 150,
+          maxFilesize: 0.5,
+          headers: { "My-Awesome-Header": "header value" },
+          dictDefaultMessage: "<i class='upload'></i>Drag & drop ton image ici"
+      }
+    }
   }
 }
 </script>
-
-<style>
-
+// https://rowanwins.github.io/vue-dropzone/docs/dist/#/iconDemo/
+<style lang="scss">
+.upload{
+  display:inline-block;
+  width:30px;
+  height:30px;
+  background:$r-color-primary;
+}
 </style>
