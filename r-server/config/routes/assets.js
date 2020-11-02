1054ab09d095d2cf6948c86bf5c9d87ef74a526a
r4ph3rd <hav4na@protonmail.com>
2020-11-02 22:52:58 +0100

    serving static files with parameters & regex to find right folder
    serving a folder wasn"t useful in this case

--- r-server/config/routes/assets.js
+++ r-server/config/routes/assets.js
@@ -2,11 +2,28 @@ const assetsQueries = require('../queries/assets');
 module.exports = [
     {
         method: 'GET',
-        path: '/assets/1/{file*}',
+        path: '/assets/dir/{file*}',
         handler: {
-            directory:{
-                path: './data/users/1/images',
-                listing: true
+            directory : {
+                path : ['data/users/1/images', 'data/users/1/audio']
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/assets/{user}/{file}',
+        handler: (request, h) => {
+            let audioExt = ['mp3', 'wav'];
+            let imageExt = ['png', 'jpg', 'gif', 'jpeg'];
+            audioExt = audioExt.map (audio => new RegExp(audio, 'i'))
+            imageExt = imageExt.map (img => new RegExp(img, 'i'))
+
+            if (audioExt.some( audio => audio.test(request.params.file))){
+                return h.file(`data/users/${request.params.user}/audio/${request.params.file}`);
+            } else if (imageExt.some( img => img.test(request.params.file))){
+                return h.file(`data/users/${request.params.user}/images/${request.params.file}`);
+            } else {
+                return h.file('404.html').code(404);
             }
         }
     },
