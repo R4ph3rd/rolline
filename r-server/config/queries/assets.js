1d37fcc7c50d05ee3c7890f657e8344d967217be
r4ph3rd <hav4na@protonmail.com>
2020-11-02 23:28:58 +0100

    getfile, have to fix the path generator for files in upload func

--- r-server/config/queries/assets.js
+++ r-server/config/queries/assets.js
@@ -5,6 +5,15 @@ const getAssets = async (userID) => {
     return await db.select().from('assets').where('owner_id', userID);
 }
 
+const getAsset = async ({userID, assetID, label}) => {
+    if (assetID){
+        return await db.select().from('assets').where({owner_id: userID, id : assetID});
+    }
+    if (label){
+        return await db.select().from('assets').where({owner_id: userID, label : label});
+    }
+}
+
 const uploadAsset = ({userID, file, label}) => {
     let path = helpers.uploadFile(file, `data/users/${userID}/${helpers.findType(file.hapi.filename)}`);
 
@@ -15,5 +24,6 @@ const uploadAsset = ({userID, file, label}) => {
 
 module.exports = {
     getAssets,
+    getAsset,
     uploadAsset
 }
