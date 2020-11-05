f76613d7eca49818ba6af0b9a99f22842c49919c
r4ph3rd <hav4na@protonmail.com>
2020-11-05 18:13:11 +0100

    add cookie in user_connect

--- r-server/config/routes/user.js
+++ r-server/config/routes/user.js
@@ -60,6 +60,18 @@ module.exports = [
                             query: query
                         }
                     } else if (rep.length == 1){
+                        const cookie_options = { // TODO : en principe, je règle un cookie avec le token ici
+                            ttl: 365 * 24 * 60 * 60 * 1000, // expires a year from today
+                            encoding: 'none',    // we already used JWT to encode
+                            isSecure: true,      // warm & fuzzy feelings
+                            isHttpOnly: true,    // prevent client alteration
+                            clearInvalid: false, // remove invalid cookies
+                            strictHeader: true   // don't allow violations of RFC 6265
+                        }
+                        let userToken = token.sign(query, process.env.SECRET_KEY);
+                        h.header("Authorization", userToken)
+                        .state("token", userToken, cookie_options);
+
                         return {
                             statusCode : 200,
                             token : token.sign(query, process.env.SECRET_KEY)
