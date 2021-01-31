b2e01032a7b35b5880d8eb0a47e7f4e45ce0a2ce
r4ph3rd <hav4na@protonmail.com>
2021-01-31 20:09:13 +0100

    add creation tables to rt config

--- r-server/rethink-config/index.js
+++ r-server/rethink-config/index.js
@@ -1,16 +1,33 @@
 const rethink = require('rethinkdb');
 const conn = rethink.connect({ host: 'localhost', port: process.env.RT_PORT || 28015 }, async function(error, connection){
-    // initialization
+    // initialization DB
     if (await rethink.dbList().contains(process.env.RT_DB_NAME).run(connection)){
         console.log('Db is already settled.')
     } else {
-        let db = await rethink.dbList().contains(process.env.RT_DB_NAME).run(connection);
-        if(!db){
-            rethink.dbCreate(process.env.RT_DB_NAME).run(connection, (err, res) => {
-                if(err) throw err;
-                console.log('New database created ! ', process.env.RT_DB_NAME);
-            })
-        }
+        rethink.dbCreate(process.env.RT_DB_NAME).run(connection, async (err, res) => {
+            if(err) throw err;
+            console.log('New database created ! ', process.env.RT_DB_NAME);
+        })
+    }
+    
+    //initialization games table
+    if (await rethink.db(process.env.RT_DB_NAME).tableList().contains('games').run(connection)){
+        console.log('Games table is already settled.')
+    } else {
+        rethink.db(process.env.RT_DB_NAME).tableCreate('games').run(connection, function(err, res) {
+            if(err) throw err;
+            console.log('games table created');
+        })
+    }
+
+    //initialization chats table
+    if (await rethink.db(process.env.RT_DB_NAME).tableList().contains('chats').run(connection)){
+        console.log('Chats table is already settled.')
+    } else {
+        rethink.db(process.env.RT_DB_NAME).tableCreate('chats').run(connection, function(err, res) {
+            if(err) throw err;
+            console.log('Chats table created');
+        })
     }
 })
 
