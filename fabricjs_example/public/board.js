4381f57e6ae84b04a1b7eb599a35345edf36d625
r4ph3rd <hav4na@protonmail.com>
2021-03-03 00:00:46 +0100

    add delete event

--- fabricjs_example/public/board.js
+++ fabricjs_example/public/board.js
@@ -5,3 +5,14 @@ const board = new fabric.Canvas('board', {
 
 // responsiveboard();
 board.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 300, left: 300 }));
+
+addEventListener('keydown', function(e){
+    console.log(e)
+    if (['Delete','Backspace'].includes(e.key)){
+        board.getActiveObjects().forEach((obj) => {
+            board.remove(obj)
+        });
+        board.discardActiveObject().renderAll()
+        console.log('object(s) deleted');
+    }
+})
