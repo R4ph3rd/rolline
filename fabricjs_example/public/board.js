59be729a2daa61a9afc32fb0e757706309d00e12
r4ph3rd <hav4na@protonmail.com>
2021-03-03 00:53:19 +0100

    add ctrl+g event to group objects

--- fabricjs_example/public/board.js
+++ fabricjs_example/public/board.js
@@ -6,6 +6,8 @@ const board = new fabric.Canvas('board', {
 // responsiveboard();
 board.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 300, left: 300 }));
 
+
+// KEYBOARD EVENTS
 addEventListener('keydown', function(e){
     console.log(e)
     if (['Delete','Backspace'].includes(e.key)){
@@ -14,5 +16,12 @@ addEventListener('keydown', function(e){
         });
         board.discardActiveObject().renderAll()
         console.log('object(s) deleted');
+
+    } else if (e.ctrlKey && e.key == 'g'){
+        if (!board.getActiveObject()) return;
+        if (board.getActiveObject().type !== 'activeSelection') return;
+
+        board.getActiveObject().toGroup();
+        board.requestRenderAll();
     }
 })
