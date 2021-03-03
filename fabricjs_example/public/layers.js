4778520670811c856158be1e145d202fffa75934
r4ph3rd <hav4na@protonmail.com>
2021-03-03 23:09:35 +0100

    keeps error on adding obejct to group, toggle selection prop of layers

--- fabricjs_example/public/layers.js
+++ fabricjs_example/public/layers.js
@@ -11,18 +11,23 @@ const layers = {
 function insertIntoLayer(e){
     console.log('insert object to layer', e.target)
 
-    layers[layers.active].add(e.target);
+    layers[layers.active].addWithUpdate(e.target);
 
 }
 
 function setActiveLayer(layer){
-    if (Object.keys(layers).filter(plop => plop != active).includes(layer)){
+    console.log('new active layer', layer)
+    if (Object.keys(layers).filter(plop => plop != layers.active).includes(layer.toLowerCase())){
         console.log('Active layer is now ', layer);
-        layers.active = layer;
+        layers.active = layer.toLowerCase();
 
-        /* Object.keys(layers).filter(plop => plop != active).forEach(group => {
-            console.log('group', group)
-            // group.selectable = false;
-        }) */
+        for (let group in layers){
+            if (group != layers.active && group != 'active'){
+                layers[group].selectable = false;
+                console.log('group', layers[group])
+            }
+        }
+
+        layers[layers.active].selectable = true;
     }
 }
