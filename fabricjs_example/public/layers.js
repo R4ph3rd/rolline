52c77ca4f7d481e77ed6b058f5c2d61de4f05d34
r4ph3rd <hav4na@protonmail.com>
2021-03-06 16:06:58 +0100

    prevent error with !this.canvas on object:added event

--- fabricjs_example/public/layers.js
+++ fabricjs_example/public/layers.js
@@ -9,23 +9,33 @@ const layers = {
 };
 
 function insertIntoLayer(e){
+    if (!this.canvas) {
+        return;
+    }
+    console.log(e.target, board)
     layers[layers.active].add(e.target);
     console.log('insert object to layer ' + layers.active, layers[layers.active])
 }
 
 function setActiveLayer(layer){
     console.log('new active layer', layer)
-    if (Object.keys(layers).filter(plop => plop != layers.active).includes(layer.toLowerCase())){
-        console.log('Active layer is now ', layer);
-        layers.active = layer.toLowerCase();
 
-        for (let group in layers){
-            if (group != layers.active && group != 'active'){
-                // layers[group].selectable = false;
-                console.log('group ' + group, layers[group])
+    if (layers[layers.active]){
+        if (Object.keys(layers).filter(plop => plop != layers.active).includes(layer.toLowerCase())){
+            console.log('Active layer is now ', layer);
+            layers.active = layer.toLowerCase();
+    
+            for (let group in layers){
+                if (group != layers.active && group != 'active'){
+                    // layers[group].selectable = false;
+                    console.log('group ' + group, layers[group])
+                }
             }
+    
+            layers[layers.active].selectable = true;
         }
-
-        layers[layers.active].selectable = true;
+    } else {
+        console.warn('No active layer selected.')
     }
+    
 }
